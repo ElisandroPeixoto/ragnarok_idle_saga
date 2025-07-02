302d6c4f8caf1d03aedcd791fa434f2cae57177e
ElisandroPeixoto <elisandropeixoto21@gmail.com>
2025-07-02 14:00:37 -0300

    dev: update screen after delete

--- views/select_char.py
+++ views/select_char.py
@@ -4,68 +4,75 @@ from components import buttons as btns
 from services.character_services import CharacterService
 
 def select_char(page: ft.Page):
-    database = SessionLocal()  # Open Database
-    characters_query = database.query(Character).all()
 
+    # Initialization
+    database = SessionLocal()
     selected_character = None
 
-    ### INTERFACE ###
 
+    ### Interface ###
     page.title = "Character Selection"
     page.bgcolor = "#E4E4E4"
 
-    # Character Cards
-    def character_card(char: Character):
-        image = "char_sprites/0.Novice_Sprite.png"
-
-        interface = ft.Container(
-            content=ft.Column(controls=[
-                    ft.Image(src=image, width=80, height=160, fit=ft.ImageFit.CONTAIN),
-                    ft.Text(char.name, size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK),
-                    ft.Text(char.job, size=14, color=ft.Colors.BLACK),
-                    ft.Text(f"Level: {char.level}", size=14, color=ft.Colors.BLACK),
-                    ft.Text(f"EXP: {char.exp}", size=14, color=ft.Colors.BLACK),
-                    ft.Text(f"HP: {char.hp}", size=14, color=ft.Colors.BLACK)
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-                tight=True,
-                spacing=2,
-                scroll=ft.ScrollMode.AUTO
-            ),
-            padding=10,
-            margin=10,
-            border_radius=10,
-            bgcolor=ft.Colors.WHITE,
-            width=180,
-            height=450,
-            alignment=ft.alignment.center,
-            shadow=ft.BoxShadow(blur_radius=5, color=ft.Colors.BLACK12, offset=ft.Offset(2, 2)),
-            border=ft.border.all(3, ft.Colors.TRANSPARENT),
-            data=char,
-            on_click=lambda e, character_selected=char: select_char(character_selected),
-        )
-
-        return interface
-
-    # Character List
-    cards = [character_card(char) for char in characters_query]
     grid = ft.GridView(
         expand=True,
         max_extent=220,
         child_aspect_ratio=0.55,
         spacing=10,
         run_spacing=10,
-        controls=cards
+        controls=[]
     )
 
+    def load_characters():
+
+        nonlocal grid
+        characters_query = database.query(Character).all()
+        
+        # Character Cards
+        def character_card(char: Character):
+            image = "char_sprites/0.Novice_Sprite.png"
+
+            interface = ft.Container(
+                content=ft.Column(controls=[
+                        ft.Image(src=image, width=80, height=160, fit=ft.ImageFit.CONTAIN),
+                        ft.Text(char.name, size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK),
+                        ft.Text(char.job, size=14, color=ft.Colors.BLACK),
+                        ft.Text(f"Level: {char.level}", size=14, color=ft.Colors.BLACK),
+                        ft.Text(f"EXP: {char.exp}", size=14, color=ft.Colors.BLACK),
+                        ft.Text(f"HP: {char.hp}", size=14, color=ft.Colors.BLACK)
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    tight=True,
+                    spacing=2,
+                    scroll=ft.ScrollMode.AUTO
+                ),
+                padding=10,
+                margin=10,
+                border_radius=10,
+                bgcolor=ft.Colors.WHITE,
+                width=180,
+                height=450,
+                alignment=ft.alignment.center,
+                shadow=ft.BoxShadow(blur_radius=5, color=ft.Colors.BLACK12, offset=ft.Offset(2, 2)),
+                border=ft.border.all(3, ft.Colors.TRANSPARENT),
+                data=char,
+                on_click=lambda e, character_selected=char: select_char(character_selected),
+            )
+
+            return interface
+
+        cards = [character_card(char) for char in characters_query]
+        grid.controls = cards
+        page.update()
+        
 
     # Character Selection
     def select_char(character: Character):
-         
+        
         nonlocal selected_character
         selected_character = character
 
-        for card in cards:
+        for card in grid.controls:
             if card.data == character:
                 card.border = ft.border.all(3, ft.Colors.BLUE)
                 card.bgcolor = ft.Colors.BLUE_50
@@ -74,14 +81,16 @@ def select_char(page: ft.Page):
                 card.bgcolor = ft.Colors.WHITE
         
         page.update()
-
+    
 
     # Character Delete
     def handle_character_delete(e):
-        if selected_character:    
+        if selected_character:
             CharacterService.delete_character(selected_character.name)
+            load_characters()
 
         page.update()
+
 
     # Buttons
     btn_new_char = btns.ElevatedButton1.apply_button(text_input="New Character", on_click_input=lambda e: page.go("/create_char"))
@@ -90,22 +99,13 @@ def select_char(page: ft.Page):
 
 
     # Interface Components Building
-    column1 = ft.Column(
-        controls=[btn_new_char, btn_delete_char],
-        spacing=20
-    )
+    column1 = ft.Column(controls=[btn_new_char, btn_delete_char], spacing=20)
+    column2 = ft.Column(controls=[ft.Container(height=45), btn_start_game], spacing=20)
+    buttons_rows = ft.Row(controls=[column1, column2], alignment=ft.MainAxisAlignment.CENTER, spacing=20)
 
-    column2 = ft.Column(
-        controls=[ft.Container(height=45), btn_start_game],
-        spacing=20
-    )
 
-    buttons_rows = ft.Row(
-        controls=[column1, column2],
-        alignment=ft.MainAxisAlignment.CENTER,
-        spacing=20
-    )
-
+    # Load the Characters Grid
+    load_characters()
 
     return ft.Column(
         controls=[ft.Container(height=20), grid, ft.Container(content=buttons_rows, alignment=ft.alignment.center, expand=True, padding=ft.padding.only(top=80))],
