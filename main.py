630f7e368076633bcba1a08ec5e4ed43d4043939
Elisandro <elisandropeixoto21@gmail.com>
2025-07-26 15:59:20 -0300

    dev: favicon and loading animation

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ def main(page: ft.Page):
     page.fonts = {"Caudex": font_caudex}
     page.theme = ft.Theme(font_family="Caudex")
 
-ft.app(target=main, assets_dir="assets")  # Dev Mode
-# ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER, port=8080)  # Dev Mode
+# ft.app(target=main, assets_dir="assets")  # Dev Mode
+ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER, port=8080)  # Dev Mode
 # ft.app(target=main, port=int(os.getenv("PORT", 8080)))  # Prod Mode
